4380fec32f4d02f31a4dce9ae22d44b3df269d65
dannytszho <dannytszho@gmail.com>
2022-04-02 23:34:06 -0700

    Add navigation

--- screens/AwareScreen.js
+++ screens/AwareScreen.js
@@ -1,15 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 
 const AwareScreen = () => {
-    const [color, setColor] = useState(null)
   return (
     <View>
       <Text>AwareScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default AwareScreen
+export default AwareScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
