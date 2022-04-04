e97479952b735db48bf6ddb7ba3ebb6791a73b90
dannytszho <dannytszho@gmail.com>
2022-04-04 02:37:26 -0700

    Add stopwatch

--- screens/StopWatchScreen.js
+++ screens/StopWatchScreen.js
@@ -1,16 +1,94 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
 
-const StopWatchScreen = () => {
-  const [time, setTime] = useState(0);
-  const [timerOn, setTimeOn] = useState(false);
-  return (
-    <View>
-      <Text>StopWatchScreen</Text>
-    </View>
-  );
+const screen = Dimensions.get("window");
+
+const formatNumber = (number) => `0${number}`.slice(-2);
+
+const getRemaining = (time) => {
+  const mins = Math.floor(time / 60);
+  const secs = time - mins * 60;
+  return { mins: formatNumber(mins), secs: formatNumber(secs) };
 };
 
-export default StopWatchScreen;
+export default function App() {
+  const [remainingSecs, setRemainingSecs] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const { mins, secs } = getRemaining(remainingSecs);
 
-const styles = StyleSheet.create({});
+  toggle = () => {
+    setIsActive(!isActive);
+  };
+
+  reset = () => {
+    setRemainingSecs(0);
+    setIsActive(false);
+  };
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setRemainingSecs((remainingSecs) => remainingSecs + 1);
+      }, 1000);
+    } else if (!isActive && remainingSecs !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, remainingSecs]);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.timerText}>{`${mins}:${secs}`}</Text>
+      <View style={styles.row}>
+        <TouchableOpacity onPress={toggle} style={styles.button}>
+          <Text style={styles.buttonText}>{isActive ? "Pause" : "Start"}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={reset} style={[styles.button]}>
+          <Text style={styles.buttonText}>Reset</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    borderWidth: 10,
+    borderColor: "gray",
+    width: screen.width / 4,
+    height: screen.width / 4,
+    borderRadius: screen.width / 2,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 100,
+    margin: 25,
+  },
+  buttonText: {
+    fontSize: 15,
+    color: "gray",
+  },
+  timerText: {
+    color: "black",
+    fontSize: 90,
+    marginBottom: 20,
+  },
+});
